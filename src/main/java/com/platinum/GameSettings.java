1b2fc8598c6d07d7166e196e83feff71cf0c1ea4
Platinum2021 <mccauleyskinner4@gmail.com>
2021-08-01 11:53:05 +0100

    Revert "test"
    This reverts commit 678ffa0b

--- src/main/java/com/platinum/GameSettings.java
+++ src/main/java/com/platinum/GameSettings.java
@@ -68,7 +68,7 @@ public class GameSettings {
 	/**
 	 * The game version
 	 */
-	public static final int GAME_VERSION = 16;
+	public static final int GAME_VERSION = 15;
 
 	/**
 	 * The maximum amount of players that can be logged in on a single game
