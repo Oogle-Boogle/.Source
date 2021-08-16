6cfb4c6130aadef0cb5d667b2ab0428311f1c32c
Platinum2021 <mccauleyskinner4@gmail.com>
2021-08-16 21:47:58 +0100

    changes dungeon to original, and added bosses for points

--- src/com.platinum/world/content/combat/bossminigame/RandomNPCData.java
+++ src/com.platinum/world/content/combat/bossminigame/RandomNPCData.java
@@ -8,34 +8,34 @@ import com.platinum.util.Misc;
 public class RandomNPCData { //CHANGE NPC'S IN HERE.. THESE ARE NPC ID'S
     //TODO Change type to Npc[]
 
-    public static final int[] firstWaveIDs = {8949, 9231, 6208}; //dragon tier
-    public static final int[] secondWaveIDs = {6595, 6599, 6603}; //mage tier (I have removed 8349)
-    public static final int[] thirdWaveIDs = {9, 6313, 6314}; //melee tier (I have removed 5666)
-    public static final int[] fourthWaveIDs = {9864, 175}; //mage tier (I have removed 2883)
-    public static final int[] fifthWaveIDs = {176}; //boss tier
+    public static final int[] firstWaveIDs = {51, 54, 50}; //dragon tier
+    public static final int[] secondWaveIDs = {3200, 2882}; //mage tier (I have removed 8349)
+    public static final int[] thirdWaveIDs = {2881, 1999}; //melee tier (I have removed 5666)
+    public static final int[] fourthWaveIDs = {499, 50}; //mage tier (I have removed 2883)
+    public static final int[] fifthWaveIDs = {3, 2001, 7134}; //boss tier
 
     public static int randomFirstWaveID() {
-        //System.out.println("RANDOM NPC 1 SELECTED");
+        System.out.println("RANDOM NPC 1 SELECTED");
         return (firstWaveIDs[Misc.getRandom(firstWaveIDs.length - 1)]);
     }
 
     public static int randomSecondWaveID() {
-        //System.out.println("RANDOM NPC 2 SELECTED");
+        System.out.println("RANDOM NPC 2 SELECTED");
         return (secondWaveIDs[Misc.getRandom(secondWaveIDs.length - 1)]);
     }
 
     public static int randomThirdWaveID() {
-        //System.out.println("RANDOM NPC 3 SELECTED");
+        System.out.println("RANDOM NPC 3 SELECTED");
         return (thirdWaveIDs[Misc.getRandom(thirdWaveIDs.length - 1)]);
     }
 
     public static int randomFourthWaveID() {
-        //System.out.println("RANDOM NPC 4 SELECTED");
+        System.out.println("RANDOM NPC 4 SELECTED");
         return (fourthWaveIDs[Misc.getRandom(fourthWaveIDs.length - 1)]);
     }
 
     public static int randomFifthWaveID() {
-        //System.out.println("RANDOM NPC 5 SELECTED");
+        System.out.println("RANDOM NPC 5 SELECTED");
         return (fifthWaveIDs[Misc.getRandom(fifthWaveIDs.length - 1)]);
     }
 
