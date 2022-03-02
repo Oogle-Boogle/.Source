75585adde5baa8df5ea373b19cd058c52b96a6f7
Oogle <istokedrs@gmail.com>
2022-03-02 01:47:43 +0000

    Teleport button

--- src/com/zamron/net/packet/impl/ButtonClickPacketListener.java
+++ src/com/zamron/net/packet/impl/ButtonClickPacketListener.java
@@ -1630,8 +1630,8 @@ public class ButtonClickPacketListener implements PacketListener {
                 break;
 
             case 10003:
-                TeleportInterface.sendMonsterData(player, TeleportInterface.Monsters.STARTER);
-                TeleportInterface.sendMonsterTab(player);
+                TeleportInterface.sendBossData(player, TeleportInterface.Slayers.BOWSER);
+                TeleportInterface.sendBossTab(player);
                 //player.getTeleportInterface().open();
                 break;
 
